49b5e67e72ca294787ad5f38ba5b1796dea3ff57
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-08-19 19:08:29 +0300

    1 in proc

---
+++ src/main/java/Controller.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.text.html.HTMLDocument;
+import java.io.File;
+
+public class Controller {
+
+    private View view; //представление
+    private HTMLDocument document; //модель
+    private File currentFile; //файл, который открыт в данный момент
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
