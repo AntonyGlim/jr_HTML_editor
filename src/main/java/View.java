fd940e5e4c00fceb96fd7feed58841ecd0cd12dc
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-08-19 19:26:21 +0300

    3.

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -8,6 +8,10 @@ public class View extends JFrame implements ActionListener {
 
     private Controller controller;
 
+    private JTabbedPane tabbedPane = new JTabbedPane(); //это будет панель с двумя вкладками
+    private JTextPane htmlTextPane = new JTextPane(); //это будет компонент для визуального редактирования html
+    private JEditorPane plainTextPane = new JEditorPane(); //это будет компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое)
+
     public void init(){}
 
     @Override
